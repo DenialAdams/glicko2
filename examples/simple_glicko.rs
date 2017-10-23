b58f2944862b77cb27891323a2d9b8fd772fa9a0
Richard McCormack <brick@brick.codes>
2017-10-23 14:27:01 -0400

    Add example usage

--- examples/simple_glicko.rs
+++ examples/simple_glicko.rs
@@ -1,6 +1,3 @@
-## Usage
-
-```rust
 extern crate glicko2;
 
 use glicko2::{GameResult, GlickoPlayer};
@@ -29,19 +26,3 @@ fn main() {
     let new_player = glicko2::new_rating(example_player, &results, 0.5);
     println!("New rating: {} New rating deviation: {}", new_player.rating, new_player.rating_deviation);
 }
-```
-
-## License
-
-Licensed under either of
-
- * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
- * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
-
-at your option.
-
-### Contribution
-
-Unless you explicitly state otherwise, any contribution intentionally submitted
-for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any
-additional terms or conditions.
