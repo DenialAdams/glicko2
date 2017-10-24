97c9a4cf0758deafb8d6235e8485e24b9e9a8274
Richard McCormack <brick@brick.codes>
2017-10-24 11:34:18 -0400

    Run rustfmt on example

--- examples/simple_glicko.rs
+++ examples/simple_glicko.rs
@@ -7,7 +7,7 @@ fn main() {
         rating: 1500.0,
         rating_deviation: 200.0,
     };
-    let mut results = vec!();
+    let mut results = vec![];
     results.push(GameResult::win(GlickoPlayer {
         rating: 1400.0,
         rating_deviation: 30.0,
@@ -20,9 +20,13 @@ fn main() {
         rating: 1700.0,
         rating_deviation: 300.0,
     }));
-    // Because we are passing in a GlickoPlayer instead of a Glicko2Player, we get a GlickoPlayer back.
+    // Because we pass in a GlickoPlayer instead of a Glicko2Player, we get a GlickoPlayer back.
     // This means that we lose the volatility field.
     // If you want to preserve volatility (i.e. use Glicko2), pass in a Glicko2Player instead.
     let new_player = glicko2::new_rating(example_player, &results, 0.5);
-    println!("New rating: {} New rating deviation: {}", new_player.rating, new_player.rating_deviation);
+    println!(
+        "New rating: {} New rating deviation: {}",
+        new_player.rating,
+        new_player.rating_deviation
+    );
 }
