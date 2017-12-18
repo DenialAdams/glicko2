2a27826b323964ea42e62d5fc49459c54d3aaf86
Richard McCormack <brick@brick.codes>
2017-12-18 18:11:23 -0500

    update terminology in example

--- examples/simple_glicko.rs
+++ examples/simple_glicko.rs
@@ -20,10 +20,10 @@ fn main() {
         value: 1700.0,
         deviation: 300.0,
     }));
-    // We are converting the result of new_rating to a GlickoPlayer immediately, throwing away the
+    // We are converting the result of new_rating to a GlickoRating immediately, throwing away the
     // benefits of Glicko2 over Glicko for the sake of matching the example in the glicko2 pdf.
-    // In a real application, you'd likely want to save the Glicko2Player and convert to
-    // GlickoPlayer for display purposes only.
+    // In a real application, you'd likely want to save the Glicko2Rating and convert to
+    // GlickoRating for display purposes only.
     let new_rating: GlickoRating = glicko2::new_rating(example_rating.into(), &results, 0.5).into();
     println!(
         "New rating value: {} New rating deviation: {}",
